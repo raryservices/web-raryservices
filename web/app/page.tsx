0d1835333837ed61fa2aa5b25b63ff015b4bd7c6
Junior331 <jarilson.rk@gmail.com>
2025-05-14 22:13:43 -0300

    feat: add framer-motion dependency and refactor Services page
    - Introduced framer-motion for enhanced animations.
    - Refactored the Services page to utilize a new layout component, removing unused code and improving structure.

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/src/presentation/layout/Navbar";
 import Hero from "@/src/presentation/layout/sections/Home/HeroSection";
-import { Logo } from "@/src/presentation/shared/Logo";
 
 export default function Home() {
   return (
